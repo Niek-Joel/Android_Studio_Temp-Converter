23948f515924d969eca4e566c647697a34c043e8
Joel Cain <fcftreetop@gmail.com>
2025-01-28 23:28:45 -0600

    Basic functionality working. Negative number input not added yet.

--- app/src/main/java/com/example/android_studio_temp_converter/MainActivity.java
+++ app/src/main/java/com/example/android_studio_temp_converter/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.android_studio_temp_converter;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.android_studio_temp_converter.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
     private double celsiusValue, fahrenheitValue;
     private ActivityMainBinding binding;
+    private EditText fahrenheitEdit, celsiusEdit;
+    private String fahrenheitText, celsiusText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,34 +26,57 @@ public class MainActivity extends AppCompatActivity {
         setContentView(view);
     }
 
+    private void setValue() {
+        try{
+            celsiusEdit = (EditText)findViewById(R.id.celsiusEdit);
+            celsiusText = celsiusEdit.getText().toString().trim();
+            if (!celsiusText.isEmpty()) {
+                celsiusValue = Double.parseDouble(celsiusText);
+            }
+
+            fahrenheitEdit = (EditText) findViewById(R.id.fahrenheitEdit);
+            fahrenheitText = fahrenheitEdit.getText().toString().trim();
+            if (!fahrenheitText.isEmpty()) {
+                fahrenheitValue = Double.parseDouble(fahrenheitText);
+            }
+        }
+        catch(Exception error){
+            Log.e(TAG, error.toString());
+            error.printStackTrace();
+        }
+    }
+
+    @SuppressLint("DefaultLocale")
     public void onClickConvert(View view) {
-
+        setValue();
         // Neither text entries filled
-//        if () {
-//
-//        }
-//
-//        // Celsius value filled
-//        else if (binding.fahrenheitEdit.equals("")) {
-//
-//        }
-//        // Fahrenheit value filled
-//        else if () {
-//
-//        }
-        // Both filled
-
+        if (celsiusText.isEmpty() && fahrenheitText.isEmpty()){
+            Toast toast = Toast.makeText(this, "Please enter a value in either fields.",Toast.LENGTH_LONG);
+            toast.show();
+        }
+        // Celsius filled, fahrenheit empty. Convert c to f and display.
+        else if (!celsiusText.isEmpty() && fahrenheitText.isEmpty()) {
+            fahrenheitValue = celsToFahren(celsiusValue);
+            binding.fahrenheitEdit.setText(String.valueOf(String.format("%.5g%n", fahrenheitValue)));
+        }
+        // Fahrenheit filled, celsius empty. OR both are filled. Convert f to c and display.
+        else if ( (!fahrenheitText.isEmpty() && celsiusText.isEmpty()  )  ||
+                  (!fahrenheitText.isEmpty() && !celsiusText.isEmpty() ) ) {
+            celsiusValue = fahrenToCels(fahrenheitValue);
+            binding.celsiusEdit.setText(String.valueOf(String.format("%.5g%n", celsiusValue)));
+        }
+        // Do nothing
+        else {
+            Toast toast = Toast.makeText(this, "I'm sorry, I don't understand.",Toast.LENGTH_LONG);
+            toast.show();
+        }
     }
 
     private double fahrenToCels(double f){
-        double c;
-        c = (f - 32) * ((double) 5/9);
-        return c;
+        return ((f-32.0)*5)/9.0;
     }
 
     private double celsToFahren(double c){
-        double f;
-        f = c * ((double) 9 /5) + 32;
-        return f;
+        return (c * (9.0/5)) + 32.0;
     }
 }
